32bed553b9e8d896c250db059d43866d1d211689
tg <eat.some.trouble@gmail.com>
2024-11-21 03:21:49 +0300

    initial commit

---
+++ src/test/java/itmo/tg/spring_boot_crud/SpringBootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package itmo.tg.spring_boot_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
