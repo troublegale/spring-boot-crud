a351fe2aabfe5fec915d5fcbec3aa10a6b44493a
tg <eat.some.trouble@gmail.com>
2024-12-05 19:27:53 +0300

    added LabWorkRepository

--- src/main/java/ru/itmo/tg/springbootcrud/labwork/repository/LabWorkRepository.java
+++ src/main/java/ru/itmo/tg/springbootcrud/labwork/repository/LabWorkRepository.java
@@ -1,9 +1,9 @@
 package ru.itmo.tg.springbootcrud.labwork.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.itmo.tg.springbootcrud.labwork.model.LabWork;
 
 @Repository
-public interface LabWorkRepository extends CrudRepository<LabWork, Long> {
+public interface LabWorkRepository extends JpaRepository<LabWork, Long> {
 }
