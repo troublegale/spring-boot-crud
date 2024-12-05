bbe8c698c3953c92b722ad6960218c8f6f73227a
tg <eat.some.trouble@gmail.com>
2024-12-05 19:24:53 +0300

    deleted RoleChangeRequestRepository.java

--- src/main/java/ru/itmo/tg/springbootcrud/security/repository/RoleChangeRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.itmo.tg.springbootcrud.security.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.itmo.tg.springbootcrud.security.model.RoleChangeRequest;
-
-public interface RoleChangeRequestRepository extends CrudRepository<RoleChangeRequest, Long> {
-}
