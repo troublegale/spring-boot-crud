29dcba98197645784a9988cf2ae0ece968f98c73
tg <eat.some.trouble@gmail.com>
2024-12-07 06:20:59 +0300

    deleted DisciplineRepository.java

--- src/main/java/ru/itmo/tg/springbootcrud/labwork/repository/DisciplineRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.itmo.tg.springbootcrud.labwork.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.itmo.tg.springbootcrud.labwork.model.Discipline;
-
-@Repository
-public interface DisciplineRepository extends JpaRepository<Discipline, Long> {
-}
