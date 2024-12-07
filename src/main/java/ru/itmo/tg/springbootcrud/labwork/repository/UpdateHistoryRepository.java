e73a798885c2aaecfcd46fc3c4a699421fe87669
tg <eat.some.trouble@gmail.com>
2024-12-07 06:22:57 +0300

    added UpdateHistoryRepository

--- src/main/java/ru/itmo/tg/springbootcrud/labwork/repository/UpdateHistoryRepository.java
+++ src/main/java/ru/itmo/tg/springbootcrud/labwork/repository/UpdateHistoryRepository.java
@@ -2,8 +2,7 @@ package ru.itmo.tg.springbootcrud.labwork.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.itmo.tg.springbootcrud.labwork.model.Coordinates;
+import ru.itmo.tg.springbootcrud.labwork.model.UpdateHistory;
 
 @Repository
-public interface CoordinatesRepository extends JpaRepository<Coordinates, Long> {
-}
+public interface UpdateHistoryRepository extends JpaRepository<UpdateHistory, Long> {}
