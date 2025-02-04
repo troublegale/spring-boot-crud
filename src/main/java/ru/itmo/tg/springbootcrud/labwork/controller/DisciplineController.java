075a4d129913fd42bd037c566380f78f945e7c81
tg <eat.some.trouble@gmail.com>
2025-02-04 17:06:34 +0300

    discipline service + controller

---
+++ src/main/java/ru/itmo/tg/springbootcrud/labwork/controller/DisciplineController.java
@@ -0,0 +1,45 @@
+package ru.itmo.tg.springbootcrud.labwork.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.itmo.tg.springbootcrud.labwork.dto.DisciplineDTO;
+import ru.itmo.tg.springbootcrud.labwork.service.DisciplineService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/disciplines")
+@RequiredArgsConstructor
+public class DisciplineController {
+
+    private final DisciplineService disciplineService;
+
+    @GetMapping
+    public List<DisciplineDTO> getDisciplines(
+            @RequestParam(defaultValue = "1") Integer page,
+            @RequestParam(defaultValue = "20", name = "page-size") Integer pageSize,
+            @RequestParam(defaultValue = "asc") String order,
+            @RequestParam(defaultValue = "id") String sort) {
+        return disciplineService.getDisciplines(page, pageSize, order, sort);
+    }
+
+    @GetMapping("/{id}")
+    public DisciplineDTO getDisciplineById(@PathVariable Long id) {
+        return disciplineService.getDisciplineById(id);
+    }
+
+    @PostMapping("/create")
+    public void createDiscipline(@RequestBody DisciplineDTO disciplineDTO) {
+        disciplineService.createDiscipline(disciplineDTO);
+    }
+
+    @PutMapping("/{id}/update")
+    public void updateDiscipline(@PathVariable Long id, @RequestBody DisciplineDTO disciplineDTO) {
+        disciplineService.updateDiscipline(id, disciplineDTO);
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public void deleteDiscipline(@PathVariable Long id) {
+        disciplineService.deleteDiscipline(id);
+    }
+}
---
+++ src/main/java/ru/itmo/tg/springbootcrud/labwork/service/DisciplineService.java
@@ -0,0 +1,45 @@
+package ru.itmo.tg.springbootcrud.labwork.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import ru.itmo.tg.springbootcrud.labwork.dto.DisciplineDTO;
+import ru.itmo.tg.springbootcrud.labwork.model.Discipline;
+import ru.itmo.tg.springbootcrud.labwork.repository.DisciplineRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DisciplineService {
+
+    private final DisciplineRepository disciplineRepository;
+    private final ModelDTOConverter modelDTOConverter;
+
+    public List<DisciplineDTO> getDisciplines(Integer pageNumber, Integer pageSize, String order, String sortCol) {
+        Page<Discipline> page = disciplineRepository.findAll(PageRequest.of(
+                pageNumber - 1, pageSize, Sort.by(Sort.Direction.fromString(order), sortCol)));
+        return modelDTOConverter.toDisciplineDTOList(page.getContent());
+    }
+
+    public DisciplineDTO getDisciplineById(Long id) {
+        return modelDTOConverter.convert(disciplineRepository.findById(id).orElseThrow());
+    }
+
+    public void createDiscipline(DisciplineDTO disciplineDTO) {
+        Discipline discipline = modelDTOConverter.convert(disciplineDTO);
+        disciplineRepository.save(discipline);
+    }
+
+    public void updateDiscipline(Long id, DisciplineDTO disciplineDTO) {
+        disciplineDTO.setId(id);
+        disciplineRepository.save(modelDTOConverter.convert(disciplineDTO));
+    }
+
+    public void deleteDiscipline(Long id) {
+        disciplineRepository.deleteById(id);
+    }
+
+}
